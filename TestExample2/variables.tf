cdbb73e8983f2d92c7178db4efc705d10ebbb1ed
root <root@ip-172-31-22-14.ec2.internal>
2024-04-13 17:55:15 +0000

    Backend

---
+++ TestExample2/variables.tf
@@ -0,0 +1,18 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "ami_id" {
+  type = map
+  default = {
+    us-east-1    = "ami-051f8a213df8bc089"
+  }
+}
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+variable "key_name" {
+  type    = string
+  default = "vm_private_key"
+}
